6b741ff3f655acf36074068e8d31f2ea3d470203
Caleb Francisco Vicente Moreno <cvicentm@everis.com>
2020-04-01 21:45:38 +0200

    1 de abril obtención de los subtitulos limpios de las noticias con logs de archivos vacios

--- get_data.py
+++ get_data.py
@@ -7,6 +7,17 @@ Este es un archivo temporal.
 
 import os
 import re
+import logging, logging.handlers
+from datetime import datetime
+#import function_get_data
+
+
+name_log_file = datetime.now().strftime('logs\load_subtitles_%d_%m_%Y.log')
+
+logging.basicConfig(filename=name_log_file, level=logging.WARNING, 
+                    format="%(asctime)s:%(filename)s:%(lineno)d:%(levelname)s:%(message)s")
+
+
 
 
 #PROGRAM:
@@ -23,6 +34,19 @@ for r, d, f in os.walk(path):
 
 
 dic_subtitles={}
+list_subtitles=[]
+#Creation of a file with all the corpus information, this file will give us information related to
+#how many of the news are empty
+
+#os.remove("file_corpus.txt")
+try:
+    os.remove("file_corpus.txt")
+except FileNotFoundError :
+    print("File file_corpus.txt has been created")
+
+
+file_corpus=open("file_corpus.txt", "w",encoding="utf-8")
+
 
 for subtitle in files:
     
@@ -31,25 +55,27 @@ for subtitle in files:
             f=open(subtitle, "r",encoding="utf-8")
             if f.mode == 'r':
                 contents = f.read()
-                dic_subtitles[subtitle]=get_news(contents)
+                #creation of a dictionary whith the content of the news
+                dic_subtitles[subtitle+"morning_new"], dic_subtitles[subtitle+"afternoon_new"]=get_news(subtitle, contents)
+                if dic_subtitles[subtitle+"morning_new"]=="":
+                    logging.warning("El subtitulo: "+subtitle+"morning_new está vacio \n")
+                if dic_subtitles[subtitle+"afternoon_new"]=="":
+                    logging.warning("El subtitulo: "+subtitle+"afternoon_new está vacio \n")
+                    
+                #creation of a list whith the content of the news
+                #MAYBE I SHOUDNT CALL GET_NEWS(CONTENT), BECAUSE I ALREADY HAVE THE INFORMATION IN THE DICTIONARY
+                #list_subtitles.append(get_news(subtitle, contents))
+                
+                file_corpus.write(subtitle+"morning_new"+"\n")
+                file_corpus.write("----------------------------------------------------------------\n")
+                file_corpus.write(dic_subtitles[subtitle+"morning_new"])
+                file_corpus.write("----------------------------------------------------------------\n")
+                file_corpus.write(subtitle+"afternoon_new"+"\n")
+                file_corpus.write("----------------------------------------------------------------\n")
+                file_corpus.write(dic_subtitles[subtitle+"afternoon_new"])
+                file_corpus.write("----------------------------------------------------------------\n")
+            f.close()
+
+file_corpus.close()
 
 
-
-#FUNCTIONS:
-#---------------------------------------------------------------------------------------
-
-def get_news(subtitles_day):
-    #I have to put differents hours depends on the channel where i catch subtitles
-    news_start=subtitles_day.find("15:00:")
-    news_end=subtitles_day.rfind("15:40:")
-    new=subtitles_day[news_start:news_end]
-    new = clean_subtitles(new)
-    return new   
-
-def clean_subtitles(subtitle):
-    #Maybe in this function i have to delete spaces between diferent sentences, because they are all separated because of the times of subtitles           
-    
-    subtitle_without_tag=re.sub('<[^>]+>', '', subtitle)
-    subtitle_without_time=re.sub('[[@*&?].*[$@*]?]', '', subtitle_without_tag)
-    
-    return subtitle_without_time
